6a0c2156433538d3dcf1f852bd6f768efb506913
cyclexit <cyclexit@outlook.com>
2021-01-09 11:11:26 -0500

    Finish the declaration of the mqtt::SubClient class

---
+++ mqtt/sub_client.hpp
@@ -0,0 +1,32 @@
+#ifndef MQTT_SUB_CLIENT_HPP_
+#define MQTT_SUB_CLIENT_HPP_
+
+#include <string>
+
+extern "C" {
+#include <mosquitto.h>
+}
+
+#include "client.hpp"
+
+namespace mqtt {
+
+class SubClient : Client {
+ private:
+  std::string topic;
+
+  void on_message(mosquitto* m, void* obj, const mosquitto_message* msg);
+
+ public:
+  SubClient(std::string _id, std::string _topic);
+  ~SubClient() {};
+
+  int connect(std::string host, int port, int duration) override;
+  int disconnect() override;
+  int start();
+  int stop(bool force);
+};
+
+} // namespace mqtt
+
+#endif
